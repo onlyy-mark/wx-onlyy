16f050670c06b3be3597b2fd2a857e0bfadd3c79
unknown <hzb@yetingit.com>
2022-06-24 15:19:15 +0800

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const logger = require("koa-logger");
 const bodyParser = require("koa-bodyparser");
 const fs = require("fs");
 const path = require("path");
-const { init: initDB, Counter } = require("./db");
+const { init: initDB, Counter, userInfo, Message } = require("./db");
 
 const router = new Router();
 
@@ -15,34 +15,105 @@ router.get("/", async (ctx) => {
   ctx.body = homePage;
 });
 
-// 更新计数
-router.post("/api/count", async (ctx) => {
-  const { request } = ctx;
-  const { action } = request.body;
-  if (action === "inc") {
-    await Counter.create();
-  } else if (action === "clear") {
-    await Counter.destroy({
-      truncate: true,
-    });
-  }
 
-  ctx.body = {
-    code: 0,
-    data: await Counter.count(),
-  };
+// 新增用户信息
+router.post("/api/updatauserInfo", async (ctx) => {
+  const { request } = ctx;
+  let { user_info } = request.body;
+  console.log("request======>", userInfo)
+
+  const { user_name, gender, avatarUrl, phone_number, country, province, city } = user_info;
+
+
+  await userInfo.updateOne({
+    user_name, gender, avatarUrl, phone_number, country, province, city
+  }).then(rel => {
+    if (rel) {
+      if (rel.modifiedCount > 0) {
+        ctx.body = {
+          code: 200,
+          msg: '用户信息修改成功',
+
+        }
+      } else {
+        await userInfo.create();
+        ctx.body = {
+          code: 300,
+          msg: '用户信息新增成功',
+          rel
+        }
+      }
+    }
+  }).catch(err => {
+    ctx.body = {
+      code: 500,
+      msg: '用户信息修改/新增时出现异常',
+      err
+    }
+  });
+
+  // // await userInfo.create();
+
+  // ctx.body = {
+  //   code: 200,
+  //   data: await Counter.count(),
+  // };
 });
 
-// 获取计数
-router.get("/api/count", async (ctx) => {
-  const result = await Counter.count();
+// 更新通知信息
+router.post("/api/updataMessage", async (ctx) => {
+  const { request } = ctx;
+  const { title, message } = request.body;
+
+  await userInfo.updateOne({
+    title, message
+  }).then(rel => {
+    if (rel) {
+      if (rel.modifiedCount > 0) {
+        ctx.body = {
+          code: 200,
+          msg: '通知信息修改成功',
+
+        }
+      } else {
+        await userInfo.create();
+        ctx.body = {
+          code: 300,
+          msg: '通知信息新增成功',
+          rel
+        }
+      }
+    }
+  }).catch(err => {
+    ctx.body = {
+      code: 500,
+      msg: '通知信息修改/新增时出现异常',
+      err
+    }
+  });
+});
+
+// 获取通知信息
+router.get("/api/getMessage", async (ctx) => {
+  const result = {
+    title: await Message.title(),
+    message: await Message.message(),
+  };
 
   ctx.body = {
-    code: 0,
+    code: 200,
     data: result,
   };
 });
 
+
+
+
+
+
+
+
+//==============================================================
 // 小程序调用，获取微信 Open ID
 router.get("/api/wx_openid", async (ctx) => {
   if (ctx.request.headers["x-wx-source"]) {
