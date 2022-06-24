4381e982e37a32854355992ab11ebb836e75201c
unknown <hzb@yetingit.com>
2022-06-24 15:47:47 +0800

    update

--- index.js
+++ index.js
@@ -108,7 +108,33 @@ router.get("/api/getMessage", async (ctx) => {
 
 
 
+// 更新计数
+router.post("/api/count", async (ctx) => {
+  const { request } = ctx;
+  const { action } = request.body;
+  if (action === "inc") {
+    await Counter.create();
+  } else if (action === "clear") {
+    await Counter.destroy({
+      truncate: true,
+    });
+  }
 
+  ctx.body = {
+    code: 0,
+    data: await Counter.count(),
+  };
+});
+
+// 获取计数
+router.get("/api/count", async (ctx) => {
+  const result = await Counter.count();
+
+  ctx.body = {
+    code: 0,
+    data: result,
+  };
+});
 
 
 
